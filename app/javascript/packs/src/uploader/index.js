bc8af0b2159ddc4082a9a5b7a46f3200fb274c08
Shivang Nagaria <shivang.nagaria@gmail.com>
2021-01-02 18:06:57 +0530

    Added Semantic UI React libs

--- app/javascript/packs/src/uploader/index.js
+++ app/javascript/packs/src/uploader/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Button } from 'semantic-ui-react'
 
 function Uploader() {
   return(
     <div>
       Uploader
+      <Button>Click Here</Button>
     </div>
   )
 }
